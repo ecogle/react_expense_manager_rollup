92dfdb02364e78fa7d9c7925f6615aad16623e3e
ecogle <ecogle@gmail.com>
2022-07-09 08:50:18 -0500

    added a class component

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+const date = new Date().toTimeString();
+
+
 class HelloWorld extends React.Component{
     render(){
         return (
             <div>
                 <h1>Hello World!</h1>
+                <p>The time is {date}</p>
             </div>
         );
     }
